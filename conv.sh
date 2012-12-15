7175ce481bbd5b286859b78f75e9fd2edb8b0006
Shihyu Yao <yaoshihyu@gmail.com>
2012-12-15 23:59:14 +0800

    cbook

--- conv.sh
+++ conv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -type f \( -name "*.htm" -o -name "*.txt" \)|while read line;do
+find . -type f \( -name "*.html" -o -name "*.txt" \)|while read line;do
 echo $line
 cconv -f utf-8 -t utf8-tw $line -o ${line}.zh_TW
 mv $line ${line}.zh_CN
